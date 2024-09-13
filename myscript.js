bdfd5c2450ff28ee050fd3822a32690d047694f9
Romulus0805 <emb_14@icloud.com>
2024-09-13 13:31:13 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
             $(".imag").click(function () {
                 $(".Outer").toggle("swing");
             });
+            $(".correct").click(function (){
+$(this).css("color", "green")();
+});
+$(".wrong").click(function (){
+$(this).css("color", "red")();
+});
         }); 
